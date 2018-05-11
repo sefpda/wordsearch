4a325823bd6e67da1126df056e3cccdbe1ddbe03
Sean Frost <sean.e.frost@gmail.com>
2018-05-11 10:26:31 -0400

    implemented vertical search

--- src/org/kata/WordSearch.java
+++ src/org/kata/WordSearch.java
@@ -1,6 +1,5 @@
 package org.kata;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -32,10 +31,52 @@ public class WordSearch {
     }
 
     private String coordinates(String word) {
+        String horizontalResult = horizontalCoordinates(word);
+        if (horizontalResult != null) return horizontalResult;
+        return verticalResult(word);
+    }
+
+    private String verticalResult(String word) {
+        boolean foundWord = false;
+        int[] x = new int[this.grid.length];
+        int[] y = new int[this.grid.length];
+        for (int i = 0; i < this.grid.length; i++) {
+            char[] column = new char[this.grid.length];
+
+            for (int j = 0; j < this.grid.length; j++) {
+                column[j] = this.grid[j].charAt(i);
+            }
+
+            String columnString = new String(column);
+            if (columnString.contains(word)) {
+                foundWord = true;
+                for (int j = 0; j < word.length(); j++) {
+                    x[j] = i;
+                    y[j] = columnString.indexOf(word) + j;
+                }
+                break;
+            }
+        }
+        StringBuilder result = new StringBuilder(word);
+        result.append(": ");
+        for (int i = 0; i < word.length(); i++) {
+            if (i > 0) result.append(",");
+            result.append("(");
+            result.append(x[i]);
+            result.append(",");
+            result.append(y[i]);
+            result.append(")");
+        }
+        return result.toString();
+    }
+
+    private String horizontalCoordinates(String word) {
+        boolean foundWord = false;
         int[] x = new int[this.grid.length];
         int[] y = new int[this.grid.length];
         for (int i = 0; i < this.grid.length; i++) {
             if (this.grid[i].contains(word)) {
+                foundWord = true;
                 for (int j = 0; j < word.length(); j++) {
                     x[j] = this.grid[i].indexOf(word) + j;
                     y[j] = i;
@@ -43,7 +84,7 @@ public class WordSearch {
                 break;
             }
         }
-
+        if (!foundWord) return null;
         StringBuilder result = new StringBuilder(word);
         result.append(": ");
         for (int i = 0; i < word.length(); i++) {
--- test/org/kata/WordSearchTest.java
+++ test/org/kata/WordSearchTest.java
@@ -26,4 +26,15 @@ public class WordSearchTest {
         assertEquals("CAP: (1,2),(2,2),(3,2)", results.get(2));
     }
 
+    @Test
+    public void findsVerticalMatches() {
+        String file = "inputfiles/verticalgrid.txt";
+        WordSearch searcher = new WordSearch(file);
+        List<String> results = searcher.getResults();
+        assertEquals("JOHN: (2,2),(2,3),(2,4),(2,5)", results.get(0));
+        assertEquals("RICK: (5,1),(5,2),(5,3),(5,4)", results.get(1));
+        assertEquals("ANDREA: (4,0),(4,1),(4,2),(4,3),(4,4),(4,5)", results.get(2));
+        assertEquals("EUGENE: (0,0),(0,1),(0,2),(0,3),(0,4),(0,5)", results.get(3));
+    }
+
 }
