7d33b6f8b02da08ae36dec22f2924d82b9345431
Sean Frost <sean.e.frost@gmail.com>
2018-06-18 15:40:11 -0400

    made duplication in forward diagonal searching more obvious

--- src/main/java/org/kata/DownwardDiagonalSearcher.java
+++ src/main/java/org/kata/DownwardDiagonalSearcher.java
@@ -19,21 +19,26 @@ class DownwardDiagonalSearcher extends AbstractSearcher {
     List<Result> findCoordinates(String[] rows, String word) {
         int[] x = new int[rows.length];
         int[] y = new int[rows.length];
+
+        short deltaY = 1;
+        int startRow = 0;
+        int limitY = rows.length - 1;
+
         List<Result> results = new ArrayList<Result>();
-        for (int startY = 0; startY < rows.length - 1; startY++) {
+        for (int startY = startRow; continueWhileY(startY, limitY); startY += deltaY) {
             for (int startX = 0; startX < rows.length - 1; startX++) {
-                if (startY > 0 && startX > 0) continue; // after first row only start at x of 0
+                if (shouldSkip(startX, startY, rows)) continue;
                 char[] letters = new char[rows.length];
-                for (int i = 0; i < letters.length && startX + i < letters.length && startY + i < letters.length; i++) {
-                    letters[i] = rows[startY + i].charAt(startX + i);
+                for (int i = 0; continueCollectingWhile(startX, startY, i, letters); i++) {
+                    letters[i] = rows[rowIndex(startY, i)].charAt(startX + i);
                 }
                 String row = new String(letters);
                 if (row.contains(word)) {
                     int xOffset = row.indexOf(word) + startX;
-                    int yOffset = row.indexOf(word) + startY;
+                    int yOffset = yOffset(startY, word, row);
                     for (int j = 0; j < word.length(); j++) {
                         x[j] = j + xOffset;
-                        y[j] = j + yOffset;
+                        y[j] = applyOffsetY(yOffset, j);
                     }
                     results.add(new Result(word, x, y));
                     x = new int[rows.length];
@@ -42,6 +47,30 @@ class DownwardDiagonalSearcher extends AbstractSearcher {
             }
         }
         return results;
+    }
+
+    private boolean continueWhileY(int y, int limit) {
+        return y < limit;
+    }
+
+    private boolean shouldSkip(int x, int y, String[] rows) {
+        return y > 0 && x > 0;
+    }
+
+    private boolean continueCollectingWhile(int x, int y, int i, char[] letters) {
+        return i < letters.length && x + i < letters.length && y + i < letters.length;
+    }
+
+    private int rowIndex(int y, int i) {
+        return y + i;
+    }
+
+    private int yOffset(int y, String word, String row) {
+        return row.indexOf(word) + y;
+    }
+
+    private int applyOffsetY(int offset, int index) {
+        return index + offset;
     }
 
 }
--- src/main/java/org/kata/UpwardDiagonalSearcher.java
+++ src/main/java/org/kata/UpwardDiagonalSearcher.java
@@ -19,21 +19,26 @@ class UpwardDiagonalSearcher extends AbstractSearcher {
     List<Result> findCoordinates(String[] rows, String word) {
         int[] x = new int[rows.length];
         int[] y = new int[rows.length];
+
+        short deltaY = -1;
+        int startRow = rows.length - 1;
+        int limitY = 0;
+
         List<Result> results = new ArrayList<Result>();
-        for (int startY = rows.length - 1; startY > 0; startY--) {
+        for (int startY = startRow; continueWhileY(startY, limitY); startY += deltaY) {
             for (int startX = 0; startX < rows.length - 1; startX++) {
-                if (startY < rows.length - 1 && startX > 0) continue; // only go past x 0 for last row
+                if (shouldSkip(startX, startY, rows)) continue;
                 char[] letters = new char[rows.length];
-                for (int i = 0; i < letters.length && startX + i < letters.length && startY - i > -1; i++) {
-                    letters[i] = rows[startY - i].charAt(startX + i);
+                for (int i = 0; continueCollectingWhile(startX, startY, i, letters); i++) {
+                    letters[i] = rows[rowIndex(startY, i)].charAt(startX + i);
                 }
                 String row = new String(letters);
                 if (row.contains(word)) {
                     int xOffset = row.indexOf(word) + startX;
-                    int yOffset = startY - row.indexOf(word);
+                    int yOffset = yOffset(startY, word, row);
                     for (int j = 0; j < word.length(); j++) {
                         x[j] = j + xOffset;
-                        y[j] = yOffset - j;
+                        y[j] = applyOffsetY(yOffset, j);
                     }
                     results.add(new Result(word, x, y));
                     x = new int[rows.length];
@@ -42,6 +47,30 @@ class UpwardDiagonalSearcher extends AbstractSearcher {
             }
         }
         return results;
+    }
+
+    private int yOffset(int y, String word, String row) {
+        return y - row.indexOf(word);
+    }
+
+    private boolean shouldSkip(int startX, int startY, String[] rows) {
+        return startY < rows.length - 1 && startX > 0;
+    }
+
+    private boolean continueWhileY(int y, int limit) {
+        return y > limit;
+    }
+
+    private boolean continueCollectingWhile(int x, int y, int i, char[] letters) {
+        return i < letters.length && x + i < letters.length && y - i > -1;
+    }
+
+    private int rowIndex(int y, int i) {
+        return y - i;
+    }
+
+    private int applyOffsetY(int offset, int index) {
+        return offset - index;
     }
 
 }
